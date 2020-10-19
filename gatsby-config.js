c292f69aebb26ab2ca5c21c70550a9273fab5f93
Cem Ergin <birazgecemuzigi@gmail.com>
2020-10-18 22:53:47 -0400

    Hello World v1.1

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Portfolio Template`,
-    description: `Portfolio template for software engineers. `,
+    description: `Portfolio template for software engineers created using Gatsby & React.`,
     author: `author name`,
   },
   plugins: [
